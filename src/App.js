9651b9e3e5691759fea292df4cc145887ea879ae
Alejandro Roa <alejandro.roa@globalesm.com>
2021-10-31 12:03:07 -0400

    init and input config

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import { Formik, Form } from 'formik';
+import Input from './components/Input';
+
+const Container = styled.div`
+display: flex;
+justify-content: center;
+height: 100%;
+align-items: center;
+`
+
+const Section = styled.section`
+background-color: #eee;
+border-top: solid 2px blue;
+padding: 20px 25px;
+width: 500px;
+box-shadow: 0px 2px 3px rgb(0,0,0,0.3);
+`
+
+function App() {
+  const handleSubmit = () => {
+
+  }
+  return (
+    <Container>
+      <Section>
+        <Formik
+          initialValues={{
+            deposit: '',
+            contribution: '',
+            years: '',
+            rate: ''
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <Input name="deposit" label="Deposito inicial"/>
+            <Input name="contribution" label="Contribución anual"/>
+            <Input name="years" label="Años"/>
+            <Input name="rate" label="Interés estimado"/>
+          </Form>
+        </Formik>
+      </Section>
+    </Container>
+  )
+}
+
+export default App;
